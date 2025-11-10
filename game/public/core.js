8e0f9468023a94e4dac62c7028dfba9c981107bf
Hancel.Lin <imlinhanchao@gmail.com>
2025-11-10 17:10:05 +0800

    📃 docs: 更新 README 与注释

--- game/public/core.js
+++ game/public/core.js
@@ -4,6 +4,9 @@ class GameCore extends Tiaoom {
     this.address = address;
   }
 
+  /**
+   * 连接服务器
+   */
   connect() {
     this.socket = new ReconnectingWebSocket(this.address, null, {debug: true, reconnectInterval: 3000});
     this.socket.onopen = () => {
@@ -23,38 +26,62 @@ class GameCore extends Tiaoom {
     };
   }
 
+  /**
+   * 关闭连接
+   */
   close() {
     this.socket.close();
   }
 
+  /**
+   * 发送消息
+   * @param {string} type 消息类型 
+   * @param {object} data 消息内容
+   * @returns 
+   */
   send({ type, data }) {
     this.socket.send(JSON.stringify({ type, data }));
     return this;
   }
 
+  /**
+   * 发送聊天消息
+   * @param {string} message 消息内容
+   * @param {string} roomId 房间ID
+   */
   say(message, roomId) {
     this.command(roomId, { type: 'say', data: message });
   }
 
-  talked(roomId) {
-    this.command(roomId, { type: 'talked' });
-  }
-
-  voted(roomId, playerId) {
-    this.command(roomId, { type: 'voted', data: { id: playerId } });
-  }
-
+  /**
+   * 初始化玩家游戏状态
+   * @param {string} roomId 房间ID
+   * @param {object} player 玩家信息
+   * @returns 
+   */
   init(roomId, player) {
     this.send({ type: "room.player-command", data: { id: roomId, type: 'status', data: player } });
     return this;
   }
 
+  /**
+   * 房间指令监听
+   * @param {function} cb 监听函数
+   * @param {boolean} on 开启/关闭监听
+   * @returns 
+   */
   onCommand(cb, on=true) {
     this.onPlayerCommand(cb, on);
     this.onRoomCommand(cb, on);
     return this;
   }
 
+  /**
+   * 消息监听
+   * @param {function} cb 监听函数
+   * @param {boolean} on 开启/关闭监听
+   * @returns 
+   */
   onMessage(cb, on=true) {
     this.onPlayerMessage(cb, on);
     this.onRoomMessage(cb, on);
--- game/public/tiaoom.js
+++ game/public/tiaoom.js
@@ -36,6 +36,9 @@ class Tiaoom {
     this.currentPlayer = null;
   }
 
+  /**
+   * 启动游戏
+   */
   run() {
     this.connect();
     this.onReady(() => {
@@ -89,20 +92,37 @@ class Tiaoom {
     return this;
   }
 
+  /**
+   * 连接服务器实现
+   */
   connect() {
     throw new Error('Must be implement connect method');
   }
 
+  /**
+   * 发送消息实现
+   */
   send({ type, data }) {
     throw new Error('Must be implement send method');
   }
 
+  /**
+   * 登录
+   * @param {Player} player 玩家信息
+   */
   login(player) {
     this.currentPlayer = new Player(player);
     this.send({ type: "player.login", data: player });
     return this;
   }
 
+  /**
+   * 创建房间
+   * @param {string} name 房间名称
+   * @param {number} size 房间人数上限
+   * @param {number} minSize 房间人数下限
+   * @param {object} attrs 房间属性
+   */
   createRoom({ name, size, minSize, attrs }) {
     return new Promise((resolve) => {
       this.send({ type: "room.create", data: { name, size, minSize, attrs } });
@@ -112,26 +132,48 @@ class Tiaoom {
     });
   }
 
+  /**
+   * 加入房间
+   * @param {string} roomId 房间ID
+   */
   joinRoom(roomId) {
     this.send({ type: "player.join", data: { roomId } });
     return this;
   }
 
+  /**
+   * 离开房间
+   * @param {string} roomId 房间ID
+   */
   leaveRoom(roomId) {
     this.send({ type: "player.leave", data: { roomId } });
     return this;
   }
 
+  /**
+   * 开始游戏
+   * @param {string} id 房间ID
+   */
   startGame(id) {
     this.send({ type: "room.start", data: { id } });
     return this;
   }
 
+  /**
+   * 准备/取消准备
+   * @param {string} roomId 房间ID
+   * @param {boolean} isReady 是否准备
+   */
   ready(roomId, isReady=true) {
     this.send({ type: isReady ? "player.ready" : "player.unready", data: { roomId } });
     return this;
   }
 
+  /**
+   * 发送房间指令
+   * @param {string} roomId 房间ID
+   * @param {object} command 指令内容
+   */
   command(roomId, command) {
     if (typeof roomId != 'string') {
       command = roomId;
@@ -142,51 +184,101 @@ class Tiaoom {
     return this;
   }
 
+  /**
+   * 连接准备监听
+   * @param {function} cb 监听函数
+   */
   onReady(cb) {
     return this.on("sys.ready", cb);
   }
 
+  /**
+   * 玩家列表变更监听
+   * @param {function} cb 监听函数
+   */
   onPlayerList(cb) {
     this.on("onPlayerList", cb);
     return this;
   }
 
+  /**
+   * 房间列表变更监听
+   * @param {function} cb 监听函数
+   * @param {boolean} on 开启/关闭监听
+   * @returns 
+   */
   onRoomList(cb, on=true) {
     if (on) this.on("onRoomList", cb);
     else this.off("onRoomList", cb);
     return this;
   }
 
+  /**
+   * 玩家加入监听
+   * @param {function} cb 监听函数
+   * @param {boolean} on 开启/关闭监听
+   * @returns 
+   */
   onPlayerJoin(cb, on=true) {
     if (on) this.on("room.join", cb);
     else this.off("room.join", cb);
     return this;
   }
 
+  /**
+   * 玩家离开监听
+   * @param {function} cb 监听函数
+   * @param {boolean} on 开启/关闭监听
+   * @returns 
+   */
   onPlayerLeave(cb, on=true) {
     if (on) this.on("room.leave", cb);
     else this.off("room.leave", cb);
     return this;
   }
 
+  /**
+   * 玩家准备监听
+   * @param {function} cb 监听函数
+   * @param {boolean} on 开启/关闭监听
+   * @returns 
+   */
   onPlayerReady(cb, on=true) {
     if (on) this.on("room.player-ready", cb);
     else this.off("room.player-ready", cb);
     return this;
   }
 
+  /**
+   * 玩家取消准备监听
+   * @param {function} cb 监听函数
+   * @param {boolean} on 开启/关闭监听
+   * @returns 
+   */
   onPlayerUnready(cb, on=true) {
     if (on) this.on("room.player-unready", cb);
     else this.off("room.player-unready", cb);
     return this;
   }
 
+  /**
+   * 玩家状态更新监听
+   * @param {function} cb 监听函数
+   * @param {boolean} on 开启/关闭监听
+   * @returns 
+   */
   onPlayerStatus(cb, on=true) {
     if (on) this.on("player.status", cb);
     else this.off("player.status", cb);
     return this;
   }
 
+  /**
+   * 房间开始游戏监听
+   * @param {function} cb 监听函数
+   * @param {boolean} on 开启/关闭监听
+   * @returns 
+   */
   onRoomStart(cb, on=true) {
     if (!this.listeners["room.start"]) {
       this.on("room.start", (room) => this.emit("onRoomStart", new Room(room)));
@@ -196,6 +288,12 @@ class Tiaoom {
     return this;
   }
 
+  /**
+   * 房间结束游戏监听
+   * @param {function} cb 监听函数
+   * @param {boolean} on 开启/关闭监听
+   * @returns
+   */
   onRoomEnd(cb, on=true) {
     if (!this.listeners["room.end"]) {
       this.on("room.end", (room) => this.emit("onRoomEnd", new Room(room)));
@@ -205,6 +303,12 @@ class Tiaoom {
     return this;
   }
 
+  /**
+   * 房间全部准备监听
+   * @param {function} cb 监听函数
+   * @param {boolean} on 开启/关闭监听
+   * @returns 
+   */
   onRoomAllReady(cb, on=true) {
     if (!this.listeners["room.all-ready"]) {
       this.on("room.all-ready", (room) => this.emit("onRoomAllReady", new Room(room)));
@@ -214,36 +318,72 @@ class Tiaoom {
     return this;
   }
 
+  /**
+   * 房间消息监听
+   * @param {function} cb 监听函数
+   * @param {boolean} on 开启/关闭监听
+   * @returns 
+   */
   onRoomMessage(cb, on=true) {
     if (on) this.on("room.message", cb);
     else this.off("room.message", cb);
     return this;
   }
 
+  /**
+   * 玩家消息监听
+   * @param {function} cb 监听函数
+   * @param {boolean} on 开启/关闭监听
+   * @returns 
+   */
   onPlayerMessage(cb, on=true) {
     if (on) this.on("player.message", cb);
     else this.off("player.message", cb);
     return this;
   }
 
+  /**
+   * 房间指令监听
+   * @param {function} cb 监听函数
+   * @param {boolean} on 开启/关闭监听
+   * @returns 
+   */
   onRoomCommand(cb, on=true) {
     if (on) this.on("room.command", cb);
     else this.off("room.command", cb);
     return this;
   }
 
+  /**
+   * 玩家指令监听
+   * @param {function} cb 监听函数
+   * @param {boolean} on 开启/关闭监听
+   * @returns 
+   */
   onPlayerCommand(cb, on=true) {
     if (on) this.on("player.command", cb);
     else this.off("player.command", cb);
     return this;
   }
 
+  /**
+   * 事件监听
+   * @param {string} event 事件名称
+   * @param {function} listener 监听函数
+   * @returns 
+   */
   on(event, listener) {
     this.listeners[event] = this.listeners[event] || [];
     this.listeners[event].push(listener);
     return this;
   }
 
+  /**
+   * 事件取消监听
+   * @param {string} event 事件名称
+   * @param {function} listener 监听函数
+   * @returns 
+   */
   off(event, listener) {
     const listeners = this.listeners[event] || [];
     const index = listeners.indexOf(listener);
@@ -253,6 +393,12 @@ class Tiaoom {
     return this;
   }
 
+  /**
+   * 事件触发
+   * @param {string} event 事件名称
+   * @param  {...any} args 参数
+   * @returns 
+   */
   emit(event, ...args) {
     const listeners = this.listeners[event] || [];
     listeners.forEach(listener => listener(...args));

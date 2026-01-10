d583dd6c7e5232381db782eed1917f16278b1754
Hancel.Lin <imlinhanchao@gmail.com>
2026-01-10 16:50:23 +0800

    🐞 fix(飞行棋): 修正移动节点刷新会导致无法继续

--- game/backend/src/games/aeroplane-chess.ts
+++ game/backend/src/games/aeroplane-chess.ts
@@ -40,6 +40,7 @@ export interface AeroplaneGameState {
   consecutiveSixes: number;
   players: Record<string, AeroplanePlayerState>;
   winnerPlayerId?: string;
+  moveable?: Record<string, (0 | 1 | 2 | 3)[]>;
 }
 
 export const name = '飞行棋';
@@ -278,6 +279,7 @@ export default class AeroplaneChessRoom extends GameRoom {
       timestamp: Date.now(),
     });
     this.state.phase = 'waiting-move';
+    this.state.moveable = { [sender.id]: movable };
     this.room.emit('command', { type: 'aeroplane:roll', data: { playerId: sender.id, roll, movable } });
     this.broadcastState();
   }
